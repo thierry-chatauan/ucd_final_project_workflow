cfb5ca48f8199fa14242adcec7cfd540ba8d02a8
thierry chatauan <thycbs@gmail.com>
2026-01-31 11:55:51 +0000

    updated cors_trusted_Oorigins

--- backend/config/deployment_settings.py
+++ backend/config/deployment_settings.py
@@ -5,7 +5,11 @@ from .settings import BASE_DIR
 
 
 ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
+CSRF_TRUSTED_ORIGINS = [
+    'https://' + os.environ.get("RENDER_EXTERNAL_HOSTNAME"),
+    "https://ucd-final-project-workflow.onrender.com",
+]
+
 
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
