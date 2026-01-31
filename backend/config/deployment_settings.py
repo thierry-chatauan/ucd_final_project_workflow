4ef86ecece42a3a5e1a555bae706efb3cb70c80c
thierry chatauan <thycbs@gmail.com>
2026-01-31 11:49:17 +0000

    cors cganged

--- backend/config/deployment_settings.py
+++ backend/config/deployment_settings.py
@@ -24,9 +24,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://ucd-final-project-workflow.onrender.com",
+]
 
 STORAGES = {
     "default": {
--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = (import.meta.env.VITE_API_URL || "").replace(/\/$/, "");
+
 
 export async function apiClient(endpoint, options = {}) {
   const token = localStorage.getItem("accessToken");
